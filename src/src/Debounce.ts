0c11a85397ec79ccd9936efff11655366d19d1d7
alexa <alex@targoninc.com>
2025-10-05 18:45:00 +0200

    be a bit smarter about debounce :)

--- src/src/Debounce.ts
+++ src/src/Debounce.ts
@@ -1,6 +1,11 @@
 const debounceMap: Record<any, NodeJS.Timeout> = {};
 
-export function debounce(identifier: any, func: () => void, delay = 500) {
+export function debounce(identifier: any, func: () => void, delay: number = 0) {
+    if (!delay) {
+        func();
+        return;
+    }
+
     if (debounceMap[identifier]) {
         clearTimeout(debounceMap[identifier]);
     }
