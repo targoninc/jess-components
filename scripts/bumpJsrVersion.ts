fbab478592684e52dc1bab68bfa5325798c37690
alex <alex@targoninc.com>
2026-05-19 17:51:32 +0200

    fix releases

--- scripts/bumpJsrVersion.ts
+++ scripts/bumpJsrVersion.ts
@@ -1,5 +1,10 @@
 import jsr from '../jsr.json';
 
-const old = jsr.version.split('.')
-jsr.version = `${old[0]}.${old[1]}.${parseInt(old[2]!) + 1}`;
+const version = Bun.argv[2];
+if (version) {
+    jsr.version = version;
+} else {
+    const old = jsr.version.split('.');
+    jsr.version = `${old[0]}.${old[1]}.${parseInt(old[2]!) + 1}`;
+}
 await Bun.write('./jsr.json', JSON.stringify(jsr, null, 2));
